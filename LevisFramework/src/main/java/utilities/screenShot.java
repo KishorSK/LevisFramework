04b869d531a47c06983c2c33da1e0bf48aed73a2
kisho <kisho@192.168.1.36>
2025-05-03 14:10:35 +0530

    ss folder creation

--- LevisFramework/src/main/java/utilities/screenShot.java
+++ LevisFramework/src/main/java/utilities/screenShot.java
@@ -13,7 +13,7 @@ public class screenShot {
 	public static String captureScreenshot(WebDriver driver,String testName)
 	{
 		File srsScreenshot =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		String dest = System.getProperty("user.dir")+"\\screenshot"+testName+".png";
+		String dest = System.getProperty("user.dir")+"\\test-output\\screenshot\\ss"+testName+".png";
 		try {
 			FileHandler.copy(srsScreenshot, new File(dest));
 		} catch (IOException e) {
