aaa6fc23420086b2839fe38ffd43357c3d240d56
KishorSK <123144600+KishorSK@users.noreply.github.com>
2025-05-13 15:13:06 +0530

    action

---
+++ LevisFramework/src/main/java/action/ActionClass.java
@@ -0,0 +1,21 @@
+package action;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionClass 
+{
+	WebDriver driver;
+	Actions action;
+	
+	public ActionClass(WebDriver driver) {
+		this.driver = driver;
+		this.action = new Actions(driver);
+	}
+	
+	public void mouseHover(WebElement element)
+	{
+		action.moveToElement(element).build().perform();
+	}
+}
